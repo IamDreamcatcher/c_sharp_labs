7a53af3920c0f000697106bcd09448d7f29a2b1a
IamDreamcatcher <OlegVolkovskiy2002@yandex.ru>
2021-03-19 16:12:45 +0300

    fix index

---
+++ 3.1Human/Humans.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Human
+{
+    class Humans {
+        int numberOfHumans = 0;
+        private Dictionary<int, Human> People;
+        public Human()
+        {
+            People = new Dictionary<int, Human>();
+        }
+        public Humans this[int index] {
+            get
+            {
+                if (index < 0 && index >= numberOfHumans)
+                {
+                    throw new Exception("There is no Human with such id\n");
+                }
+                return People[index];
+            }
+            set 
+            {
+                if (index != value.Id)
+                {
+                    throw new Exception("There is no vehicle with such id\n");
+                }
+                
+                People[value.Id] = value;
+                numberOfHumans++;
+            }
+        }
+    }
+}
